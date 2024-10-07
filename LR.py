b12a6d6b13f0833702ef3ea60965a4c72f69df62
nitishadow <iamnitishsah12@gmail.com>
2024-10-07 13:55:19 +0530

    3rd Commit

--- LR.py
+++ LR.py
@@ -31,32 +31,33 @@ w = 0
 b = 0
 k = 0.001
 
+
 def gradient(x, y, m, w, b):
     dj_dw = 0
     dj_db = 0
-
     for i in range(m):
         f_wb_xi = w * x[i] + b
 
         dj_dw += f_wb_xi - y[i]
         dj_db += (f_wb_xi - y[i]) * x[i]
-
     dj_db /= m
     dj_dw /= m
-
     return dj_dw, dj_db
+
 
 def gradient_descent(m, w, b, k, iterations):
     for i in range(iterations):
         dj_db, dj_dw = gradient(x, y, m, w, b)
         w = w - k * dj_dw
         b = b - k * dj_db
-
     return w, b
+
 
 w, b = gradient_descent(m, w, b, k, iterations)
 
+
 f_wb_x = w * x + b
+
 
 plt.scatter(x, y, color='blue', label = 'Given data')
 plt.plot(x, f_wb_x, color='red', label = 'Fitted line')
@@ -67,6 +68,7 @@ plt.show()
 def prediction(exp, w, b):
     exp = float(exp)
     return w * exp + b
+
 
 exp = input("Your Experience: ")
 predicted_salary = prediction(exp, w, b)
