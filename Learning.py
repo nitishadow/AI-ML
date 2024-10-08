ce0d1bbe7707086d8c6878d58333cd147681ede5
nitishadow <iamnitishsah12@gmail.com>
2024-10-08 14:45:46 +0530

    Stop tracking Learning.py Rough.py

--- Learning.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-dataset = pd.read_csv('Fake Bills Data.csv', sep = ';')
-X = dataset.iloc[:, :-1].values
-y = dataset['is_genuine'].map({True:1, False:0}).values
-
